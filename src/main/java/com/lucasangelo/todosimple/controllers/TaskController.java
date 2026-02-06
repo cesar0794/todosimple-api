811e029894ce9c1834fa024cb191c8638e3898d2
Cesar Garcia <c.cesarhg1@gmail.com>
2026-02-06 01:30:29 -0300

    Refactor TaskController

--- src/main/java/com/lucasangelo/todosimple/controllers/TaskController.java
+++ src/main/java/com/lucasangelo/todosimple/controllers/TaskController.java
@@ -20,6 +20,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lucasangelo.todosimple.models.Task;
 import com.lucasangelo.todosimple.services.TaskService;
+import com.lucasangelo.todosimple.services.UserService;
 
 @RestController
 @RequestMapping("/task")
@@ -30,10 +31,20 @@ public class TaskController {
     @Autowired
     private TaskService taskService;
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/{id}")
     public ResponseEntity<Task> findById(@PathVariable Long id) {
         Task obj = this.taskService.findById(id);
         return ResponseEntity.ok(obj);
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<Task>> findAllByUserId(@PathVariable Long userId) {
+        this.userService.findById(userId);
+        List<Task> objs = this.taskService.findAllByUserId(userId);
+        return ResponseEntity.ok().body(objs);
     }
 
     @PostMapping
@@ -61,9 +72,4 @@ public class TaskController {
 
     }
 
-    @GetMapping("/user/{userId}")
-    public ResponseEntity<List<Task>> findAllByUserId(@PathVariable Long userId) {
-        List<Task> objs = this.taskService.findAllByUserId(userId);
-        return ResponseEntity.ok().body(objs);
-    }
 }
