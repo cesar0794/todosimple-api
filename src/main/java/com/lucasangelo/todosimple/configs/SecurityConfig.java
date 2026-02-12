12603a4091f35e1ccb91d652becf735fb404e362
Cesar Garcia <c.cesarhg1@gmail.com>
2026-02-12 02:11:06 -0300

    Create JWTAuthenticationFilter

--- src/main/java/com/lucasangelo/todosimple/configs/SecurityConfig.java
+++ src/main/java/com/lucasangelo/todosimple/configs/SecurityConfig.java
@@ -7,18 +7,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import com.lucasangelo.todosimple.security.JWTAuthenticationFilter;
 import com.lucasangelo.todosimple.security.JWTUtil;
 
 @Configuration
@@ -26,58 +26,55 @@ import com.lucasangelo.todosimple.security.JWTUtil;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
-    private AuthenticationManager authenticationManager;
+        @Autowired
+        private JWTUtil jwtUtil;
 
-    @Autowired
-    private UserDetailsService userDetailsService;
+        // Injetamos a configuração de autenticação do Spring Boot
+        @Autowired
+        private AuthenticationConfiguration authenticationConfiguration;
 
-    @Autowired
-    private JWTUtil jwtUtil;
+        private static final String[] PUBLIC_MATCHERS = {
+                        "/"
+        };
 
-    private static final String[] PUBLIC_MATCHERS = {
-            "/"
+        private static final String[] PUBLIC_MATCHERS_POST = {
+                        "/user",
+                        "/login"
+        };
 
-    };
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-    private static final String[] PUBLIC_MATCHERS_POST = {
-            "/user",
-            "/login"
+                http.cors().and().csrf().disable();
 
-    };
+                // Recuperamos o AuthenticationManager da configuração padrão do Spring
+                AuthenticationManager authenticationManager = authenticationConfiguration.getAuthenticationManager();
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+                http.authorizeRequests(requests -> requests
+                                .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
+                                .antMatchers(PUBLIC_MATCHERS).permitAll()
+                                .anyRequest().authenticated());
 
-        http.cors().and().csrf().disable();
+                // Passamos o authenticationManager recuperado para o filtro
+                http.addFilter(new JWTAuthenticationFilter(authenticationManager, this.jwtUtil));
 
-        AuthenticationManagerBuilder authenticationManagerBuilder = http
-                .getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.userDetailsService(this.userDetailsService)
-                .passwordEncoder(bCryptPasswordEncoder());
-        this.authenticationManager = authenticationManagerBuilder.build();
+                http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-        http.authorizeRequests(requests -> requests
-                .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
-                .antMatchers(PUBLIC_MATCHERS).permitAll()
-                .anyRequest().authenticated());
+                return http.build();
+        }
 
-        http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        return http.build();
-    }
+        @Bean
+        CorsConfigurationSource corsConfigurationSource() {
+                CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+                configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE"));
+                final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                source.registerCorsConfiguration("/**", configuration);
+                return source;
+        }
 
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
-        configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE"));
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+        @Bean
+        public BCryptPasswordEncoder bCryptPasswordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
 
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    // aaa
-};
+}
---
+++ src/main/java/com/lucasangelo/todosimple/security/JWTAuthenticationFilter.java
@@ -0,0 +1,68 @@
+package com.lucasangelo.todosimple.security;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lucasangelo.todosimple.exceptions.GlobalExceptionHandler;
+
+import com.lucasangelo.todosimple.models.User;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private AuthenticationManager authenticationManager;
+
+    private JWTUtil jwtUtil;
+
+    public JWTAuthenticationFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
+        setAuthenticationFailureHandler(new GlobalExceptionHandler());
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
+        try {
+            // 1. Pega os dados que vieram na requisição (JSON) e converte para a classe
+
+            User userCredentials = new ObjectMapper().readValue(request.getInputStream(), User.class);
+
+            // 2. Cria o token do Spring Security (vazio de permissões por enquanto)
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                    userCredentials.getUsername(),
+                    userCredentials.getPassword(),
+                    new ArrayList<>());
+
+            // 3. Tenta autenticar
+            Authentication authentication = this.authenticationManager.authenticate(authToken);
+            return authentication;
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected void successfulAuthentication(HttpServletRequest request,
+            HttpServletResponse response, FilterChain filterChain, Authentication authentication)
+            throws IOException, ServletException {
+        UserSpringSecurity UserSpringSecurity = (UserSpringSecurity) authentication.getPrincipal();
+        String username = UserSpringSecurity.getUsername();
+        String token = this.jwtUtil.generateToken(username); 
+
+        response.addHeader("Authorization", "Bearer " + token);
+
+    }
+
+}
